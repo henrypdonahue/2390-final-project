17d3a8285d0a864b210df058aaae5ac758ca03ec
Weili <a320_271n@outlook.com>
2023-11-15 16:05:02 -0500

    feat: input party submit data

--- input-party.js
+++ input-party.js
@@ -1,5 +1,33 @@
-const sodium = require('libsodium-wrappers-sumo');
-const { JIFFClient } = require('jiff-mpc');
+const sodium = require("libsodium-wrappers-sumo");
+const { JIFFClient } = require("jiff-mpc");
+const axios = require("axios");
+
+async function submitData(token, analystShare, serverShare) {
+  const url = "http://localhost:8080/submit";
+  axios.post(url, {
+    token: token,
+    analystShare: analystShare,
+    serverShare: serverShare
+  })
+  .then((response) => console.log("submit response: ", response.statusText))
+  .catch((error) => console.log("failed to submit data: ", error.code));
+
+}
+
+// Function to delete data
+async function deleteData(token) {
+  const url = `http://localhost:8080/delete/${token}`;
+
+  const response = await fetch(url, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    console.log("Data deleted successfully");
+  } else {
+    console.error("Failed to delete data");
+  }
+}
 
 // Read command line arguments
 async function main() {
@@ -12,18 +40,24 @@ async function main() {
   if (command === "input") {
     input = parseInt(input, 10);
 
-    const jiffClient = new JIFFClient('fakeurl', 'fakecomputation', { autoConnect: false });
-    
+    // create fake jiffclient for computing shares
+    const jiffClient = new JIFFClient("fakeurl", "fakecomputation", {
+      autoConnect: false,
+    });
+
     // send shares along with random token to server via some http request.
-    const shares = jiffClient.hooks.computeShares(jiffClient, input, [1, 's1'], 2, jiffClient.Zp)
-    const token = Buffer.from(sodium.randombytes_buf(32)).toString('base64');
-
-    console.log(shares);
-    console.log("use token to delete:", token);    
-
+    const shares = jiffClient.hooks.computeShares(
+      jiffClient,
+      input,
+      [1, "s1"],
+      2,
+      jiffClient.Zp,
+    );
+    const token = Buffer.from(sodium.randombytes_buf(32)).toString("base64");
+    submitData(token, shares[1], shares['s1']);
   } else if (command === "delete") {
     console.log("deleting token", input);
-    // send input to server to delete via some http request.
+    deleteData(input);
   }
 }
 
--- server.js
+++ server.js
@@ -1,52 +1,74 @@
 // Dependencies
-let http = require('http');
-const { JIFFServer } = require('jiff-mpc');
+let http = require("http");
+const { JIFFServer } = require("jiff-mpc");
+const express = require("express");
 
-// Create express and http servers
-const express = require('express');
-let app = express();
+const app = express();
+const port = 8080;
+
+let DB = {
+  "wnbROE838fxBgtGE59LfuNQ=": { analyst: 15789588, server: 988161 },
+  "JsOzGXTQA4hJ1QelE2g5WDc=": { analyst: 9064049, server: 7713720 },
+};
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Define an endpoint for submitting data
+app.post("/submit", (req, res) => {
+  const { token, analystShare, serverShare } = req.body;
+  DB[token] = { analyst: analystShare, server: serverShare };
+  res.status(200).json({ message: "data submitted successfully" });
+});
+
+// Define an endpoint for deleting data based on a token
+app.delete("/delete/:token", (req, res) => {
+  const { token } = req.params;
+  // Check if the token exists in the DB
+  if (DB[token]) {
+    // Delete the entry corresponding to the token
+    delete DB[token];
+    res.status(200).json({ message: "Data deleted successfully" });
+  } else {
+    res.status(404).json({ message: "Token not found" });
+  }
+});
+
 http = http.Server(app);
 
 // Create JIFF server
 const jiffServer = new JIFFServer(http, { logs: false });
-jiffServer.computationMaps.maxCount['test'] = 2;
-
-// You need to define some rest APIs here for input parties to send
-// shares and tokens, and request deletion based on a token, etc.
-// Lets say these API fill up the object DB with the submitted shares.
-const DB = {
-  // Two hypothetical submissions from input parties.
-  // Each are keyed by the random token the input party generates, so that
-  // it is easier to find / delete the shares of that party given the token.
-  // Note: the analyst share should be encrypted, this is just a demonstration.
-  // Submission for party A: these two secret shares reconstruct to 10.
-  "wnbROE838fxBgtGE59LfuNQ=": { analyst: 15789588, server: 988161 },
-  // Submission for party B: these two secret shares reconstruct to 20.
-  "JsOzGXTQA4hJ1QelE2g5WDc=": { analyst: 9064049, server: 7713720 }
-};
+jiffServer.computationMaps.maxCount["test"] = 2;
 
 // Specify the computation server code for this demo
-var computationClient = jiffServer.compute('test', { crypto_provider: true });
-computationClient.wait_for([1, 's1'], async function () {
+var computationClient = jiffServer.compute("test", { crypto_provider: true });
+computationClient.wait_for([1, "s1"], async function () {
   // Perform server-side computation.
-  console.log('Computation initialized!');
+  console.log("Computation initialized!");
 
   // Send all the shares of the analyst to the analyst (they should be encrypted)
   // Keep the shares of the server here.
   let analystShares = [];
   let serverShares = [];
   for (const [token, shares] of Object.entries(DB)) {
-    analystShares.push(shares['analyst']);
-    serverShares.push(shares['server']);
+    analystShares.push(shares["analyst"]);
+    serverShares.push(shares["server"]);
   }
 
   // send the analyst shares to the analyst (party with id = 1).
-  computationClient.emit('shares', [1], JSON.stringify(analystShares));
-  
+  computationClient.emit("shares", [1], JSON.stringify(analystShares));
+
   // turn the server shares to JIFF secret share objects.
   let shares = [];
   for (let i = 0; i < serverShares.length; i++) {
-    shares.push(new computationClient.SecretShare(serverShares[i], [1, 's1'], 2, computationClient.Zp));
+    shares.push(
+      new computationClient.SecretShare(
+        serverShares[i],
+        [1, "s1"],
+        2,
+        computationClient.Zp,
+      ),
+    );
   }
 
   // Now we can do whatever computation we want. For example, we can add the two submissions together.
@@ -56,10 +78,11 @@ computationClient.wait_for([1, 's1'], async function () {
   }
 
   // Reveal the result.
-  const output = await computationClient.open(sum, [1, 's1']);
+  const output = await computationClient.open(sum, [1, "s1"]);
   console.log("Result is", output);
 });
 
-http.listen(8080, function () {
+// Start the server
+http.listen(port, function () {
   console.log('listening on *:8080');
 });
