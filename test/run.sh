0b1107b806b6da23f46ee5bc93bce3981c5255a0
weili <willyykidd@gmail.com>
2023-12-14 19:45:39 -0500

    test: benchmarking script

--- test/run.sh
+++ test/run.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# Run the command and capture the output
-test/run_once.sh 2>&1
+input_cnt_array=(10 20 30 40 50)
+delete_cnt_array=(2 4 6 8 10)
 
-begin_time=$(awk '/begin time:/{print $NF}' .tmp.time)
-end_time=$(awk '/end time:/{print $NF}' .tmp.time)
+run_benchmark() {
+  input_cnt=$1
+  delete_cnt=$2
 
-# Calculate the runtime in seconds
-runtime=$((end_time - begin_time))
+  # run the benchmark and redirect the output to a temporary file
+  test/run_once.sh $input_cnt $delete_cnt 2>&1 > .tmp.time
+  
+  # extract the begin_time and end_time from the temporary file
+  begin_time=$(awk '/begin time:/{print $NF}' .tmp.time)
+  end_time=$(awk '/end time:/{print $NF}' .tmp.time)
+  
+  runtime=$((end_time - begin_time))
+  
+  echo "$input_cnt,$delete_cnt,$runtime" >> benchmark_results.csv
+}
 
-echo "Begin time: $begin_time"
-echo "End time: $end_time"
-echo "Runtime: $runtime seconds"
+echo "input_cnt,delete_cnt,time" > benchmark_results.csv
+
+for input_cnt in "${input_cnt_array[@]}"; do
+  for delete_cnt in "${delete_cnt_array[@]}"; do
+    run_benchmark $input_cnt $delete_cnt
+  done
+done
+
+rm -f .tmp.time
